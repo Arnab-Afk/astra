df55eb2293c4a2fc7a1b7bcd305ef2d6ee6d4303
arnab-afk <arnabbhowmik019@gmail.com>
2024-06-04 23:17:44 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <CommonLoader />
+  <div id="smooth-wrapper">
+    <div id="smooth-content">
+      <CommonNavbar />
+      <CommonMenu />
+      
+      <main class="main-bg">
+        <div class="main-box main-bg ontop">
+        <PreviewHeader />
+        <LandingWorks />
+        <LandingBlog />
+      </div>
+        <!-- <PreviewPortfolio /> -->
+      </main>
+      <PreviewFooter />
+    </div>
+  </div>
+</template>
+
+<script setup>
+//= Page Metadata
+definePageMeta({
+  layout: "preview",
+});
+//= Page Head
+useHead({
+  titleTemplate: `%s - Preview`,
+  link: [
+    {
+      rel: "stylesheet",
+      href: '/assets/css/base.css'
+    }
+  ],
+  script: [
+  { src: "/assets/js/TweenMax.min.js" },
+    { src: "/assets/js/charming.min.js" },
+    { src: "/assets/js/smoother-script.js", defer: true }
+  ]
+  
+});
+</script>
